f5aebb4446c871069ff8ef01ad6749cb349bed5d
Ropecito <ross.antho@gmail.com>
2017-09-21 15:33:01 -0600

    Add files via upload

---
+++ zero_extend.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.09.2017 15:31:24
+// Design Name: 
+// Module Name: zero_extend
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module zero_extend(zOut32,zIn16);
+output [31:0] zOut32;
+input [15:0] zIn16;
+assign zOut32 = {{16{1'b0}},zIn16};
+endmodule
