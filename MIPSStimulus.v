6d617f6c5beb02493a8afc56387acc929316faf4
Ropecito <ross.antho@gmail.com>
2017-09-21 16:45:48 -0600

    Add files via upload

---
+++ MIPSStimulus.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.09.2017 16:44:13
+// Design Name: 
+// Module Name: MIPSStimulus
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MIPSStimulus();
+// fpga4student.com: FPGA projects, Verilog Projects, VHDL projects
+// Verilog project: 32-bit 5-stage Pipelined MIPS Processor in Verilog 
+// Testbench Verilog code for 32-bit 5-stage Pipelined MIPS Processor 
+parameter ClockDelay = 5000;
+
+reg clk,reset;
+
+
+MIPSpipeline  myMIPS(clk, reset);
+initial clk = 0;
+always #(ClockDelay/2) clk = ~clk;
+
+initial 
+begin
+   reset = 1;
+  #(ClockDelay/4);
+  reset = 0;
+end
+endmodule
