c8ed0dc8fb709cb58dffdaf4b6411fcb7e30d154
Ropecito <ross.antho@gmail.com>
2017-09-21 15:39:26 -0600

    Add files via upload

---
+++ signext_shift2.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.09.2017 15:37:52
+// Design Name: 
+// Module Name: signext_shift2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module signext_shift2(sOut32,sIn16);
+output [31:0] sOut32;
+input [15:0] sIn16;
+assign sOut32 = {{16{sIn16[15]}},sIn16};
+endmodule
+// Shift left 2 module 
+module shift_left_2(Out32, In32);
+output [31:0] Out32;
+input [31:0] In32;
+
+assign Out32 = {In32[29:0],2'b00};
+endmodule
