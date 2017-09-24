800da7bdf544f3795603cd506c4e78a45ec8f5ac
egax16 <edu.gonzaleza.16.eg@gmail.com>
2017-09-24 16:32:32 -0600

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -19,6 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+//Este modulo es la unidad de control de la ALU.
 
 module ALUControl(ALUControl, ALUOp, Function);
 output [1:0] ALUControl;
@@ -26,15 +27,15 @@ reg [1:0] ALUControl;
 input [1:0] ALUOp;
 input [5:0] Function;
 wire [7:0] ALUControlIn;
-assign ALUControlIn = {ALUOp,Function};
+ assign ALUControlIn = {ALUOp,Function}; //Asigna a ALUControlIn los 2 bits de ALUOp seguido de los 6 bits de Function.
 always @(ALUControlIn)
-casex (ALUControlIn)
- 8'b11xxxxxx: ALUControl=2'b01;
+ casex (ALUControlIn) //En este case dependiendo de lo que se encuentre en ALUControlIn, pone en ALUControl cierto valor.
+  8'b11xxxxxx: ALUControl=2'b01; //En los primeros 3 caso, el valor importante es el de ALUOp y el de Function se encuentra en estado no importa
  8'b00xxxxxx: ALUControl=2'b00;
  8'b01xxxxxx: ALUControl=2'b10;
- 8'b10100000: ALUControl=2'b00;
+ 8'b10100000: ALUControl=2'b00; //En los siguientes 3 casos, si ALUOp es igual a 10, revisa el valor que se encuentra en Fuction
  8'b10100010: ALUControl=2'b10;
  8'b10101010: ALUControl=2'b11;
- default: ALUControl=2'b00;
+ default: ALUControl=2'b00; //Es el valor default de ALUControl
  endcase
 endmodule
